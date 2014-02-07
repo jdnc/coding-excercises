db2f626d55ac3d9baaa4dc2076f1a5b17715a2c1
Madhura Parikh <madhuraparikh@gmail.com>
2014-02-07 17:24:02 -0600

    codechef feb challenge1 successful attempt

--- lcp.cpp
+++ lcp.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
-#include<algorithm>
 #include<string>
-#include<vector>
+#include<set>
 
 using namespace std;
 
@@ -12,38 +11,21 @@ int main(){
 		string s1, s2;
 		int maxLen = 0;
 		cin >> s1 >> s2;
-		vector<string *>  suffix1;
-		vector<string *> suffix2;
-		sort(s1.begin(), s1.end());
-		sort(s2.begin(), s2.end());
-		for (int i = 0; i< s1.length(); i++)
-			suffix1.push_back(&s1[i])
-		for (int i = 0; i< s2.length(); i++)
-			suffix2.push_back(&s2[i]);
-		sort(suffix1.begin(), suffix1.end());
-		sort(suffix2.begin(), suffix2.end());
-		for(int i = 0; i<s1.length() && i < s2.length(); i++){
-			if(suffix1[i] == suffix2[i]){
-				if (suffix1[i]->length() > maxLen)
-					maxLen = suffix1[i]->length();
+		multiset<char> set1;
+		multiset<char> :: iterator itm;
+		string::iterator it1;
+		for(it1 = s1.begin(); it1 != s1.end(); it1++)
+			set1.insert(*it1);
+		for(it1 = s2.begin(); it1 != s2.end(); it1++){
+			itm = set1.find(*it1);
+			if (itm == set1.end())
+				continue;
+			else{
+				set1.erase(itm);
+				maxLen++;
 			}
-		}
-		while(i < s1.length()){
-			if(suffix1[i] == suffix2[s2.length() - 1]){
-				if (suffix1[i]->length() > maxLen)
-					maxLen = suffix1[i]->length();
-			}
-			i++;
-		}
-		while(i < s2.length()){
-			if(suffix2[i] == suffix1[s1.length() - 1]){
-				if (suffix2[i]->length() > maxLen)
-					maxLen = suffix2[i]->length();
-			}
-			i++;
 		}
 		cout << maxLen << endl;
-
 	}
 
 }
