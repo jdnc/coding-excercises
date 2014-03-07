da749165d6c91bdef30b8fcb3156343bf84266f0
Madhura Parikh <madhuraparikh@gmail.com>
2014-03-07 16:09:33 -0600

    not compiling with ll directive

--- littlenums.cpp
+++ littlenums.cpp
@@ -1,22 +1,22 @@
-#define ll long long
+//#define long long long long
 #include<iostream>
 
 
 using namespace std;
 
 
-ll comb2(ll n);
+long long comb2(long long n);
 
 int main(){
 	int t;
-	ll n, a;
+	long long n, a;
 	cin >> t;
-	ll num2;
-	ll numPairs;
+	long long num2;
+	long long numPairs;
 	for (int i = 0; i< t; i++){
 		cin >> n;
 		numPairs = num2 = 0;
-		for (ll j = 0; ll < n; ll++){
+		for (long long j = 0; long long < n; long long++){
 			cin   >>  a;
 			if (a == 1)
 				numPairs -= n - j -1;
@@ -30,6 +30,6 @@ int main(){
 }
 
 
-ll comb2(ll n){
+long long comb2(long long n){
 	return n*(n-1)/2;
 }
