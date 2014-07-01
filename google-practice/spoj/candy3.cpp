7079fe3d8e46c90a33a5483ad84a799d0abd86e9
Madhura <madhuraparikh@gmail.com>
2014-07-01 01:32:12 +0000

    Create candy3.cpp

---
+++ google-practice/spoj/candy3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+
+  ll n;
+  ll t;
+  ll x;
+  cin >> t;
+  for (ll k = 0; k<t ; k++) {
+	  ll sum = 0;
+	  cin >> n;
+	  for (ll i = 0; i < n; ++i) {
+	     cin >> x;
+	     sum += x;
+	  }
+	  if (sum % n == 0)
+	    cout << "YES" << endl;
+	  else 
+	    cout << "NO" << endl;
+  } 
+  return 0;
+
+}
